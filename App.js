e04afd05672e9756a88d619f3db5edbe4444c549
Vadim Klimov <vklimov@luxoft.com>
2024-11-16 19:14:04 +0200

    mapbox

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Mapbox, { MapView } from "@rnmapbox/maps";
+import { MAPBOX_PUBLIC_TOKEN } from "@env";
+
+Mapbox.setAccessToken(MAPBOX_PUBLIC_TOKEN);
+
+function App() {
+    return (
+        <MapView style={styles.map} />
+    );
 }
 
+export default App;
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    map: {
+        flex: 1
+    }
 });
